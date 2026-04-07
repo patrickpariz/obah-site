08e0e60df6e5ce7a9dcd669c41ab5862a67a7c15
patrickpariz <poubelpariz@gmail.com>
2026-04-06 21:36:19 -0300

    meta pixel

--- app/layout.js
+++ app/layout.js
@@ -72,6 +72,19 @@ const schema = {
   ]
 }
 
+const metaPixel = `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '35161662913447457');
+fbq('track', 'PageView');
+`
+
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
@@ -80,10 +93,20 @@ export default function RootLayout({ children }) {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
         />
+        <script dangerouslySetInnerHTML={{ __html: metaPixel }} />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=35161662913447457&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
+
     </html>
   )
 }
